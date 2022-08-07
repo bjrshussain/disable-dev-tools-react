b902c41ee7e631c9c420ce535012944b7e079362
bjrshussain@outlook.com <bjrshussain@outlook.com>
2022-08-06 21:05:28 -0500

    basic react app showing hwo to disable dev tools for production

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.REACT_APP_NODE_ENV === 'production') {
+  disableReactDevTools();
+}
+
+function App() {
+
+  const [email, setemail] = useState('test@gmail.com')
+  const [name, setname] = useState('Michael Scott')
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
